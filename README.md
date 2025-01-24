5004b4728e4f384ad9583a983b635589d6fb0cd1
Nicholas Chiarello <madeupmon@gmail.com>
2025-01-24 04:57:46 -0500

    working tool

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Tabletop-Sim-Digimon-Deck-Generator
-generates a json file with your deck to insert into tabletop sim
+# Deck Generator Tool for Digimon TCG Tabletop Simulator games
+
+## Instructions
+
+Go to digimoncard.dev<br>
+Create your deck in the deck editor, then export to TTS.<br>
+![The export menu of digimoncard.dev](carddev.png)<br>
+Any other deckbuilder that exports in the same format is also a valid alternative.
+
+Copy and paste the export text as written into the program when prompted to.<br>
+Copy and paste the outputted file directly into your TTS saved objects folder (On Windows this is located at Documents/My Games/Tabletop Simulator/Saves/Saved Objects).<br>
+Load it into a lobby as you would any other item.
+![The saved object as seen in the ingame menu](object.png)
+
+Optional: You can use any image you want as the in game thumbnail for your deck, by placing a .png into the Saved Objects folder with the same name as your .json file
+
+## TODO
+
+- Unreleased cards<br>
+    The images are taken from the official Digimon TCG website and thus can only use cards currently hosted on the site.<br>
+- Parallel Arts<br>
+    Due to how digimoncard.dev exports cards, another method is needed to identify parallel arts for cards
--- main.py
+++ main.py
@@ -1,13 +1,57 @@
 import sys
-import filegenerator
 
+eggs = ["-01","001","002","003","004","005","006"]
+eggstras = ["BT1-007","BT1-008","BT2-007","EX2-007","BT13-007","P-148","P-149","P-157"]
+eggceptions = ["EX1-001","EX1-002","EX1-003","EX1-004","EX1-005","EX1-006","EX3-003","EX3-004","EX3-005","EX3-006","EX4-005","EX4-006","LM-001","LM-002","LM-003","LM-004","LM-005","LM-006","RB1-004","RB1-005","RB1-006","P-001","P-002","P-003","P-004","P-005","P-006"]
+
+def filegenerator(decklist):
+    counter = 0 #number of cards in deck
+    contents = '{"ObjectStates":[{"Name":"Deck","Transform":{"posX":0,"posY":0,"posZ":0,"rotX":0,"rotY":180,"rotZ":180,"scaleX":2.35946536,"scaleY":1.0,"scaleZ":2.35946536},"Nickname":"Digimon Deck","DeckIDs":['
+    for card in decklist:
+        counter += 1
+        contents += f'{counter *100},'
+    contents = contents[:-1]
+    contents += '],"CustomDeck":{'
+
+    for i in range(counter):
+        if decklist[i][-3:] in eggs and decklist[i] not in eggceptions:
+            cardBack = "https://file.garden/ZUWEKmrAFHgJ5X5e/DigimonBackEgg.jpg"
+        elif decklist[i] in eggstras:
+            cardBack = "https://file.garden/ZUWEKmrAFHgJ5X5e/DigimonBackEgg.jpg"
+        else:
+            cardBack = "https://file.garden/ZUWEKmrAFHgJ5X5e/DigimonBack.jpg"
+        contents += f'"{i+1}":{{"FaceURL":"https://world.digimoncard.com/images/cardlist/card/{decklist[i]}.png","BackURL":"{cardBack}","NumHeight":1,"NumWidth":1,"BackIsHidden":true}},'
+
+    contents = contents[:-1]
+    contents += '},"ContainedObjects":['
+    for i in range(counter):
+        if decklist[i][-3:] in eggs and decklist[i] not in eggceptions:
+            cardBack = "https://file.garden/ZUWEKmrAFHgJ5X5e/DigimonBackEgg.jpg"
+        elif decklist[i] in eggstras:
+            cardBack = "https://file.garden/ZUWEKmrAFHgJ5X5e/DigimonBackEgg.jpg"
+        else:
+            cardBack = "https://file.garden/ZUWEKmrAFHgJ5X5e/DigimonBack.jpg"
+        contents += f'{{"Name":"CardCustom","Transform":{{"posX":0,"posY":0,"posZ":0,"rotX":0,"rotY":180,"rotZ":180,"scaleX":2.35946536,"scaleY":1.0,"scaleZ":2.35946536}},"Nickname":"{decklist[i]}", "CardID":{(i+1)*100},"CustomDeck":{{"{i+1}":{{"FaceURL":"https://world.digimoncard.com/images/cardlist/card/{decklist[i]}.png","BackURL":"{cardBack}","NumHeight":1,"NumWidth":1,"BackIsHidden":true}}}}}},'
+
+    contents = contents[:-1]
+    contents += ']}]}'
+    return contents
+
+file = "Digimon Deck.json"
 input = input("Enter Decklist: ")
+#input comes from digimoncard.dev TTS export
+
 try:
     decklist = eval(input)
 except:
     print("Invalid List")
     sys.exit()
+#converts string to list
 
-decklist.pop(0)
-print("deck:", decklist)
-filegenerator.filegenerator(decklist)
+decklist.pop(0) #first element is garbage
+jsoncontents = filegenerator(decklist)
+
+with open(file, "w") as f:
+    f.write(jsoncontents)
+
+print("Deck object generated successfully")
